6b960af4550b735bbb7bb558cdc4dae326cb15b3
Kim Sooyeon <5196ksy@naver.com>
2025-09-22 18:37:34 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        for(int i : array){
+            while(i > 0){
+                if(i % 10 == 7){
+                    answer++;
+                }
+                i /=  10;
+            }
+        }
+        
+        return answer;
+    }
+}

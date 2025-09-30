eb6029714e275a130ffa662f5d92100256947b4f
Kim Sooyeon <5196ksy@naver.com>
2025-09-30 13:42:57 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        for (String[] user : db) {
+            if (id_pw[0].equals(user[0])) {
+                return id_pw[1].equals(user[1]) ? "login" : "wrong pw";
+            }
+        }
+        return "fail";
+    }
+}

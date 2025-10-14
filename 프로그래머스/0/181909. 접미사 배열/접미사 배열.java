0cd459faf86f18c2123ca3472dc587a64d693508
Kim Sooyeon <5196ksy@naver.com>
2025-10-14 09:33:42 +0900

    [level 0] Title: 접미사 배열, Time: 2.47 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        List<String> answer = new ArrayList<>();
+        
+        for(int i = 0; i < my_string.length(); i++){
+            answer.add(my_string.substring(i,my_string.length()));
+        }
+
+        Collections.sort(answer);
+        
+        return answer.stream().toArray(String[]::new);
+    }
+}

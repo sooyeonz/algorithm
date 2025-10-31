ac40b7330955ff64379a4674bbc2441ab29d3174
Kim Sooyeon <5196ksy@naver.com>
2025-10-31 09:18:56 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.06 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int a, int b) {
+        if(a % 2 == 0) {
+            if(b % 2 == 0){
+                return Math.abs(a - b);
+            } else {
+                return 2 * (a + b);
+            }
+        } else {
+            if(b % 2 == 0){
+                return 2 * (a + b);
+            } else {
+                return (a * a) + (b * b);
+            }
+        }
+    }
+}

38b1319de3bc60c6241e482bbdf02a690e9781e9
Kim Sooyeon <5196ksy@naver.com>
2025-12-12 10:34:12 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int num) {
+        if(num == 1) {
+            return 0;
+        }
+        
+        long n = num; 
+        int answer = 0;
+        
+        while(n != 1) {
+            if(answer >= 500) {
+                return -1;
+            }
+            
+            if(n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = n * 3 + 1;
+            }
+            answer++;
+        }
+        
+        return answer;
+    }
+}

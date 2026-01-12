241305baccde3db181c12980ba19821316baa288
Kim Sooyeon <5196ksy@naver.com>
2026-01-12 18:00:17 +0900

    [level 1] Title: K번째수, Time: 1.18 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        
+        int[] answer = new int[commands.length];
+        
+        for(int a = 0; a < commands.length; a++){
+            int i = commands[a][0];
+            int j = commands[a][1];
+            int k = commands[a][2];
+            
+            int[] tempArr = Arrays.copyOfRange(array, i-1, j);
+            Arrays.sort(tempArr);
+            answer[a] = tempArr[k-1];
+        }
+        
+        return answer;
+    }
+}

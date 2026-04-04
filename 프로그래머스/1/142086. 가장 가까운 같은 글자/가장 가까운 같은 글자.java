93a1193337d996ece9ee7b7d5dec52ba72407e95
Kim Sooyeon <5196ksy@naver.com>
2026-04-04 10:27:00 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 5.12 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+        
+        Map<Character,Integer> map = new HashMap<>(); 
+        int[] answer = new int[s.length()];
+        
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            
+            if(!map.containsKey(c)){
+                answer[i] = -1;
+            } else {
+                answer[i] = i - map.get(c);
+            }
+            
+            map.put(c, i);
+        }
+        
+        return answer;
+    }
+}
